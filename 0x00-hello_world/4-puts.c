dad591c0206f5a8c4d1a6063a38b7999bf76aba4
Nastydroid <sopadefemi@outlook.com>
2022-10-13 16:09:21 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
-	puts("programming is like building a multiligual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
