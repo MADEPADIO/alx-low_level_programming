ad4a96d029b26c24cab57a3e6a976ceccad858e8
Nastydroid <sopadefemi@outlook.com>
2022-10-16 07:06:08 -0700

    print lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 97;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
+
