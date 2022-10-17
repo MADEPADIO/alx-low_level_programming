73eb633c62c326ad22091d6b1f5996f5bdd1ca48
Nastydroid <sopadefemi@outlook.com>
2022-10-17 05:24:54 -0700

    algo

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int n = 122;
 
-	while (n < 96)
+	while (n > 96)
 	{
 		putchar(n);
 		n--;
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,6 +16,7 @@ int main(void)
 	while (a < 10)
 	{
 		putchar(a + '0');
+		if (a < 9)
 		putchar(',');
 		putchar(' ');
 		a++;
