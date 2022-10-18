1632ecccfb7e4627d96e9afa757044ca75b36aec
Nastydroid <sopadefemi@outlook.com>
2022-10-18 16:27:32 -0700

    print me 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+int a;
+int b;
+
+a = 0;
+
+while (a < 11)
+{
+	b = 97;
+
+	while (b < 123)
+	{
+		_putchar(b);
+		b++;
+	}
+
+	_putchar('\n');
+	a++;
+}
+}
