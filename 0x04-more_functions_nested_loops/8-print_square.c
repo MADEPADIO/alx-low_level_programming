ce8155c745a4bf69048fb61f94b542d306e94af6
Nastydroid <sopadefemi@outlook.com>
2022-10-21 14:32:25 -0700

    sqaure up

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - a function that print square
+ *
+ * @size: this takes an integer
+ *
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+}
