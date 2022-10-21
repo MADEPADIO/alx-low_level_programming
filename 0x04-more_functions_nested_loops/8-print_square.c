9f2baf845adc283ee0990577acfdd249a746172d
Nastydroid <sopadefemi@outlook.com>
2022-10-21 14:56:07 -0700

    draw mw

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,18 +13,18 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = 1; a < size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 1; b < size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
-		} else
-		{
+
 			_putchar('\n');
 		}
+
+	} else
+	{
+		_putchar('\n');
 	}
 }
-
-
