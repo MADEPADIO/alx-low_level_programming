99ba2fce16dbb2236f968afc41cd59435934b7cc
Nastydroid <sopadefemi@outlook.com>
2022-10-25 14:46:21 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - a function that print string follwed by new line
+ *
+ * @str: input of char
+ */
+
+void _puts(char *str)
+{
+	char a = str;
+
+	_putchar(a);
+	_putchar("\n");
+}
+
