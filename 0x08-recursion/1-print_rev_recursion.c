acfbbce0a1f8c543a3d00360ac83d162b25a25cc
Nastydroid <sopadefemi@outlook.com>
2022-11-02 11:14:09 -0700

    code fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _print_rev_recursion - a function that print a string in reverse
  *
  * @s: string input
@@ -11,7 +10,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
 		_putchar('\n');
 		return;
