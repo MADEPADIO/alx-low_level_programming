d6ecb9c17d87db077c5d997c2b1b62a011588ba1
Nastydroid <sopadefemi@outlook.com>
2022-11-02 10:54:26 -0700

    recurse

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string followed by a new line
+ *
+ * @s: string input
+ *
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return ('\n');
+	}
+	_puchar(*s);
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+Recursion  is a way to solve probems without using loops
+
+required editor is VIM or EMACs
+
+coding style- Betty Style
