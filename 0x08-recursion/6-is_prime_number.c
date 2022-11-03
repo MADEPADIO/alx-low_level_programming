bfcc05aa23382b17945f8c263b0baaaa44778cae
Nastydroid <sopadefemi@outlook.com>
2022-11-02 17:55:38 -0700

    fixiing"

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,12 +10,11 @@
 
 int is_prime_number(int n)
 {
-	if (n / n == 1 || n / 1 == n)
-		return (1);
-
 	if (n < 0 || n == 1)
 		return (0);
-	return (0);
+
+	if (n / n == 1 || n / 1 == n)
+		return (1);
 }
 
 
