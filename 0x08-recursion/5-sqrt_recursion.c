ebe11c75ddab2fb0eea72715839db92b092c7fce
Nastydroid <sopadefemi@outlook.com>
2022-11-03 11:37:13 -0700

    initial commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * find_root-  a function that find square root of a number
+ *
+ * @n: input integer
+ * @root: testing root value
+ * Return: Intger
+ */
+
+int find_root(int n, int root)
+{
+	if (root * root == n)
+		return (root);
+	if (root * root > n || root * root < n)
+		return (-1);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - a function that print the  sqaure root of a number
+ *
+ * @n: input integer
+ * Return: Always an int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (find_root(n, 0));
+}
+
+
