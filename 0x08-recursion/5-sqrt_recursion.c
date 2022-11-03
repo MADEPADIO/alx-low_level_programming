e4a1bd62df9bd852fd391ad8bc4275768a3104bc
Nastydroid <sopadefemi@outlook.com>
2022-11-03 11:39:04 -0700

    code fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int find_root(int n, int root)
 {
 	if (root * root == n)
 		return (root);
-	if (root * root > n || root * root < n)
+	if (root * root > n)
 		return (-1);
 
 	return (find_root(n, root + 1));
