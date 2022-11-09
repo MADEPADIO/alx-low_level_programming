7bd224bb3c724e39be021eb09ef305f543001e14
Nastydroid <sopadefemi@outlook.com>
2022-11-09 10:51:53 -0800

    initial commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: args counters
+ * @argv: char pointer of array
+ * Return: 0 if successful else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, int sum;
+
+	if (i != 3)
+		printf("error\n");
+	return (1);
+
+	for (i = 1; i < argc; i++)
+		sum *= atoi(argv[i]);
+	printf("%d", i);
+	return (0);
+}
