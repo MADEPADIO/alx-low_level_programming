6ec4c51d9a0c13be975ca720940cbe82a6f93bae
Nastydroid <sopadefemi@outlook.com>
2022-11-09 11:05:49 -0800

    wrap up

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,13 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc != 3)
+	{
+
 		printf("error\n");
-	return (1);
+
+		return (1);
+	}
+
 
 	printf("%d\n", atoi(argv[1] * atoi(argv[2]));
 	return (0);
