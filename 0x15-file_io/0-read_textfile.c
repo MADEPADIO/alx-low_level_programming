9223a2a72ac1b837246dbd4198cbe74b84446062
Nastydroid <sopadefemi@outlook.com>
2022-12-06 14:47:15 -0800

    input outout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile -this read a text file and print the letters on screen
+ * @filename: filename
+ * @letters: number of letters printed
+ *
+ * Return: numbers of letters printed. it fails, return 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+
+	if (!buf)
+		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+	return (nwr);
+}
+
